4d29a030570d7ee8a7b5c3eff2e3dd071fc4f0e3
instcode <instcode@cb1a20be-7f77-11de-aa08-e921640d9421>
2010-01-18 15:40:46 +0000

    Support debug logging flag

--- MobileSpy/src/org/ddth/http/core/Logger.java
+++ MobileSpy/src/org/ddth/http/core/Logger.java
@@ -2,19 +2,28 @@ package org.ddth.http.core;
 
 import android.util.Log;
 
+
 public class Logger {
 	private static Logger logger = new Logger();
+
+	private boolean isDebug = true;
 	
 	public static Logger getDefault() {
 		return logger;
 	}
 
+	public boolean isDebug() {
+		return isDebug;
+	}
+	
 	public void error(String message, Throwable e) {
 		Log.e("MobileSpyService", message, e);
 	}
 
 	public void debug(String message) {
-		Log.d("MobileSpyService", message);
+		if (isDebug) {
+			Log.d("MobileSpyService", message);
+		}
 	}
 
 	public void info(String message) {
@@ -22,7 +31,9 @@ public class Logger {
 	}
 
 	public void trace(String message) {
-		Log.d("MobileSpyService", message);
+		if (isDebug) {
+			Log.d("MobileSpyService", message);
+		}
 	}
 
 }
